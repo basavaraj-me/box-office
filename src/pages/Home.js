f0adbf0998927452859b596caa34341542ca9144
Basava <basavarj7065@gmail.com>
2022-11-21 21:29:11 +0530

    created preview card for api data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,19 @@
 import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import ActorGrid from '../components/actor/ActorGrid';
+import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput]=useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState("shows");
+
+  const isShowSearch = searchOption === "shows";
   
   const onSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
-      console.log(result);
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
     });
   }
 
@@ -20,11 +27,40 @@ const Home = () => {
     }
   }
 
+  const renderResults = () => {
+
+    if(results && results.length === 0){
+      return <div>No results</div>
+    }
+
+    if(results && results.length >0){
+      return results[0].show ? <ShowGrid data={results}/> : <ActorGrid data={results}/>
+    }
+
+    return null;
+
+  }
+
+  const onRadioChange = (ev) => {
+    setSearchOption(ev.target.value);
+  }
+console.log(searchOption);
 
   return (
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+      <input type="text" placeholder='Search for somthing' onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+      <div>
+        <label htmlFor='shows-search'>
+          Show
+          <input id="shows-search" type="radio" value="shows" checked={isShowSearch} onChange={onRadioChange}/>
+        </label>
+        <label htmlFor='actors-search'>
+          Actor
+          <input id="actors-search" type="radio" value="people" checked={!isShowSearch} onChange={onRadioChange}/>
+        </label>
+      </div>
       <button type="button" onClick={onSearch} >Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
-// eslint-disable-next-line react/function-component-definition
 const Starred = () => {
   return (
     <MainPageLayout>This is STARRED Page</MainPageLayout>
