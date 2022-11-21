1173e53b9dc0ff4848fa0cdc5ccc211e05c50c26
Basava <basavarj7065@gmail.com>
2022-11-21 23:07:04 +0530

    installed styled components and created styles for search cards

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ActorCard from './ActorCard';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGride } from '../../styled';
 
 const ActorGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGride>
       {
         data.map(({person})=><ActorCard key={person.id} name={person.name} country={person.country ? person.country.name : null} birthday={person.birthday} deathday={person.deathday} gender={person.gender} image={person.image ? person.image.medium : IMAGE_NOT_FOUND}/>)
       }
-    </div>
+    </FlexGride>
   );
 };
 
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ShowCard from './ShowCard';
 import IMG_NOT_FOUND from '../../images/not-found.png';
+import { FlexGride } from '../../styled';
 
 const ShowGrid = ({data}) => {
   return (
-    <div>
+    <FlexGride>
         {
         data.map(({show})=> <ShowCard key={show.id} id={show.id} name={show.name} image={show.image ? show.image.medium : IMG_NOT_FOUND} summary={show.summary}/>)}
-    </div>
+    </FlexGride>
   )
 }
 
