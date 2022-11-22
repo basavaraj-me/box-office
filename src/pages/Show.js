036c78d5c6781a98172caf604003e0bab193037e
Basava <basavarj7065@gmail.com>
2022-11-23 01:09:58 +0530

    created show page component and added data to them

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 import React,{useEffect, useReducer} from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 // eslint-disable-next-line consistent-return
@@ -51,8 +56,22 @@ const Show = () => {
     }
 
   return (
-    <div>This is Show page</div>
-  )
+      <div>
+          <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres}/>
+          <div>
+            <h2> Details </h2>
+            <Details status={show.status} network={show.network} premiered={show.premiered}/>
+          </div>
+          <div>
+            <h2> Seasons </h2>
+            <Seasons seasons={show._embedded.seasons}/>
+          </div>
+          <div>
+            <h2> Cast </h2>
+            <Cast cast={show._embedded.cast}/>
+          </div>
+      </div>
+      )
 }
 
 export default Show;
