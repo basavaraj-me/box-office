7ada1c380e3497a6c7093d3fd1e8340987f2eba7
Basava <basavarj7065@gmail.com>
2022-11-23 00:10:31 +0530

    replaced usestate with useReducer

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,34 +1,48 @@
 /* eslint-disable no-console */
-import React,{useEffect, useState} from 'react';
+import React,{useEffect, useReducer} from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+// eslint-disable-next-line consistent-return
+const reducer = (prevState, action) => {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+        case 'FETCH_SUCCESS':{
+            return {isLoading:false, error:false, show:action.show}
+        }
+        case 'FETCH_FAILED':{
+            return {...prevState, isLoading:false, error:action.errror}
+        }
+    }
+};
+
+const initialState = {
+    show:null,
+    isLoading:true,
+    error:null
+}
+
 const Show = () => {
     const {id} = useParams();
-    const [show, setShow] = useState(null);
-    const [isloading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [{show, isLoading, error}, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
         let isMount = true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
                 if(isMount){
-                    setShow(results);
-                setIsLoading(false);
+                    dispatch({type:'FETCH_SUCCESS', show:results});
                 }
         }).catch(err =>{
             if(isMount){
-                setError(err.message);
-                setIsLoading(false);
+                dispatch({type:'FETCH_FAILED', error:err.message})
             }
         });
         return () => {
             isMount = false;
         }
-    },[id, setShow])
+    },[id])
     console.log('show', show);
-
-    if(isloading){
+    if(isLoading){
         return <div>Data is being loading</div>
     }
 
